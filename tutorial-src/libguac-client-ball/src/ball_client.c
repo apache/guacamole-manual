328396091f59155161397a730239b5e2d982d512
Michael Jumper <zhangmaike@users.sourceforge.net>
2012-08-29 13:23:01 -0700

    Added ball tutorial.

---
+++ tutorial-src/libguac-client-ball/src/ball_client.c
@@ -0,0 +1,11 @@
+
+/* Client plugin arguments */
+const char* GUAC_CLIENT_ARGS[] = { NULL };
+
+int guac_client_init(guac_client* client, int argc, char** argv) {
+
+    /* Do nothing ... for now */
+    return 0;
+
+}
+
